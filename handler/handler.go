efc1df73c8fbbc88b4d45807910900ed398c9e24
Cunjun Wang <13621691063@163.com>
2019-07-09 22:18:40 +0800

    实现简单的文件上传

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // UploadHandler: 处理文件上传
@@ -11,14 +13,42 @@ import (
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		// 返回上传html页面
-		data, err := ioutil.ReadFile("./static/view/index.html")
+		data, err := ioutil.ReadFile("./static/view/upload.html")
 		if err != nil {
 			io.WriteString(w, "internal server error")
 			return
 		}
 		io.WriteString(w, string(data))
-	} else if r.Method == "POST" {
+	} else {
 		// 接收文件流并存储到本地目录
-	}
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Failed to get data from file, err: %s", err.Error())
+			return
+		}
+		// 在函数退出之前把file流关掉
+		defer file.Close()
 
+		// 创建本地文件
+		newFile, err := os.Create("./upload/" + header.Filename)
+		if err != nil {
+			fmt.Printf("Failed to create local file, err: %s", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		// 把上传的文件流拷贝写入创建的本地文件
+		_, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("Failed to write data to local file, err: %s", err.Error())
+			return
+		}
+
+		http.Redirect(w, r, "/file/upload/success", http.StatusFound)
+	}
+}
+
+// UploadSuccessHandler: 上传完成
+func UploadSuccessHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload finished!")
 }
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
-package nixie_cloud_storage
+package main
 
 import (
 	"fmt"
 	"net/http"
+	"nixie-cloud-storage/handler"
 )
 
 func main() {
 	// 建立路由规则
-	http.HandleFunc("/file/upload", UploadHandler)
+	http.HandleFunc("/file/upload", handler.UploadHandler)
+	http.HandleFunc("/file/upload/success", handler.UploadSuccessHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("Fail to start server, err: %s", err.Error())
